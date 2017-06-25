b5954183e5d8854730177a344c06cd6ef981d79e
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-06-25 12:15:01 -0700

    Bug fix for no-such-warning

--- src/warnings/narrowing-conversions.h
+++ src/warnings/narrowing-conversions.h
@@ -28,6 +28,10 @@
 #endif
 
 #if defined(WARNINGS_HAVE_MSVC)
+// Some of these warnings aren't consistently recognized.
+// Both 2397 and 2398 aren't documented.
+#   include <warnings/no-such-warning.h>
 #   pragma warning(disable: 2397)
+#   pragma warning(disable: 2398)
 #   pragma warning(disable: 4838)
 #endif
